b4afef305118cd331c0293b8cd1881bc8b550304
Eduardo Barroso <git@edbar.xyz>
2024-04-19 22:02:52 -0300

    Adds installation script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+echo "We need sudo priviledges for installing packages. Please provide your password if is not already cached."
+sudo -v
+
+echo "Checking if yay is installed on your system..."
+command -v yay &> /dev/null || exit 1 "Yay doesn't seem to be installed."
+
+echo "Updating repositories..."
+yay -Syy
+
+echo "Installing dependencies..."
+cat ./packages | xargs yay -S --needed --noconfirm
+
+echo "You will now set up p10k"
+p10k configure
+
+echo "Linking dotfiles..."
+stow --dotfiles -v .
+
+sleep 2
+echo "We're logging you out of i3 to make sure everything is properly handled."
+
+sleep 2
+i3-msg exit
