39955d680c87c3c36c515aa7e6bdccfc791f6332
Eduardo Barroso <git@edbar.xyz>
2024-04-28 16:28:04 -0300

    adds sync option to setup script

--- setup.sh
+++ setup.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-clear
-echo "We need sudo priviledges for installing packages."
-echo "Please provide your password."
-sudo -v
+link_dotfiles() {
+	echo "Linking dotfiles..."
+	stow --dotfiles -v .
+}
 
-echo "Checking if yay is installed on your system..."
-command -v yay &> /dev/null || exit 1 "Yay doesn't seem to be installed."
+if [[ "$1" == "sync" ]]; then
+	link_dotfiles
+	if [[ "$?" == 0 ]]; then
+		echo "Dotfiles updated with no errors."
+	fi
+else
+	clear
+	echo "We need sudo priviledges for installing packages."
+	echo "Please provide your password."
+	sudo -v
 
-echo "Updating repositories..."
-yay -Syy
+	echo "Checking if yay is installed on your system..."
+	command -v yay &> /dev/null || exit 1 "Yay doesn't seem to be installed."
 
-echo "Installing dependencies..."
-cat ./packages | xargs yay -S --needed --noconfirm
-git clone https://github.com/romkatv/powerlevel10k
+	echo "Updating repositories..."
+	yay -Syy
 
-echo "You will now set up p10k"
-p10k configure
+	echo "Installing dependencies..."
+	cat ./packages | xargs yay -S --needed --noconfirm
+	git clone https://github.com/romkatv/powerlevel10k
 
-echo "Linking dotfiles..."
-stow --dotfiles -v .
+	echo "You will now set up p10k"
+	p10k configure
 
-sleep 2
-echo "We're logging you out of i3 to make sure everything is properly handled."
+	echo "Linking dotfiles..."
+	link_dotfiles
 
-sleep 2
-i3-msg exit
+	sleep 2
+	echo "We're logging you out of i3 to make sure everything is properly handled."
+
+	sleep 2
+	i3-msg exit
+fi
