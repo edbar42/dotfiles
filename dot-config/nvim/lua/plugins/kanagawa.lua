b6ad665347d16a255564a3929d92643a1da8a739
Eduardo Barroso <git@edbar.xyz>
2024-05-02 00:58:46 -0300

    change kanagawa flavour

--- dot-config/nvim/lua/plugins/kanagawa.lua
+++ dot-config/nvim/lua/plugins/kanagawa.lua
@@ -33,13 +33,13 @@ return {
 					PmenuThumb = { bg = theme.ui.bg_p2 },
 				}
 			end,
-			theme = "lotus", -- Load "wave" theme when 'background' option is not set
+			theme = "wave", -- Load "wave" theme when 'background' option is not set
 			background = { -- map the value of 'background' option to a theme
 				dark = "dragon", -- try "dragon" !
 				light = "lotus",
 			},
 		})
 		-- setup must be called before loading
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme kanagawa-wave")
 	end,
 }
