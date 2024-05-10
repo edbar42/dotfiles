a2417e3b14fba5c4f346d9035c201722847846ee
Eduardo Barroso <git@edbar.xyz>
2024-05-10 13:34:46 -0300

    minor tweaks

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -110,6 +110,7 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"gofumpt", -- Used to format Go code
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
