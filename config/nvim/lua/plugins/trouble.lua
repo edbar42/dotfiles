f68bb879f24112aac9d3f6f4300ed37447139ec8
Eduardo Barroso <dev@edbar.xyz>
2024-07-17 21:40:47 -0300

    add: simple troube.nvim setup

---
+++ config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,16 @@
+return {
+	"folke/trouble.nvim",
+	opts = {
+		auto_close = true,
+		focus = true,
+		follow = true,
+	}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+	},
+}
