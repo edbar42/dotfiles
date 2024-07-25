20febd53016ff597026af3d4055a3d7f8bc9058a
Eduardo Barroso <dev@edbar.xyz>
2024-07-25 14:22:09 -0300

    tidy: tidy kanagawa.lua

--- config/nvim/lua/plugins/kanagawa.lua
+++ config/nvim/lua/plugins/kanagawa.lua
@@ -3,23 +3,19 @@ return {
 	config = function()
 		-- Default options:
 		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
 			commentStyle = { italic = true },
 			functionStyle = { bold = true },
 			keywordStyle = { italic = true, bold = true },
 			statementStyle = { bold = true },
 			typeStyle = { bold = true },
 			transparent = true, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
 			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = { ui = { bg_gutter = "none" } } },
+				theme = { all = { ui = { bg_gutter = "none" } } },
 			},
 			overrides = function(colors) -- add/modify highlights
 				local theme = colors.theme
 				return {
+					-- Telescope colors settings
 					TelescopeTitle = { fg = theme.ui.special, bold = true },
 					TelescopePromptNormal = { bg = theme.ui.bg_p1 },
 					TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
@@ -27,11 +23,14 @@ return {
 					TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
 					TelescopePreviewNormal = { bg = theme.ui.bg_dim },
 					TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+
+					-- Completion menu settings
+					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
 					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
 					PmenuSbar = { bg = theme.ui.bg_m1 },
 					PmenuThumb = { bg = theme.ui.bg_p2 },
 
+					-- Floating window settings
 					FloatBorder = { bg = "none" },
 					FloatTitle = { bg = "none" },
 					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
@@ -39,13 +38,7 @@ return {
 					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 				}
 			end,
-			theme = "wave", -- Load "wave" theme when 'background' option is not set
-			background = { -- map the value of 'background' option to a theme
-				dark = "dragon", -- try "dragon" !
-				light = "lotus",
-			},
 		})
-		-- setup must be called before loading
 		vim.cmd("colorscheme kanagawa-wave")
 	end,
 }
