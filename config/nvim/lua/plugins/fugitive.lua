66dea34ab116cd9bc2dd499ad76ad1037700f052
Eduardo Barroso <dev@edbar.xyz>
2024-10-03 02:56:21 -0300

    trying out resize for git fugitive

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -4,11 +4,13 @@ return { -- Git wrapper for neovim
 	config = function()
 		vim.keymap.set("n", "<leader>gs", function()
 			vim.cmd("vertical Git")
+			vim.cmd("vertical resize 50")
 		end, { desc = "Git (Fugitive) status" })
 
 		vim.keymap.set("n", "<leader>df", vim.cmd.Gdiffsplit, { desc = "Open diffs on split window" })
 		vim.keymap.set("n", "<leader>gl", function()
-			vim.cmd("Git --paginate log")
+			vim.cmd("vertical Git --paginate log")
+			vim.cmd("vertical resize 65")
 		end, { desc = "Open diffs on split window" })
 	end,
 }
