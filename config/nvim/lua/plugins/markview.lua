6dd6a5e7adc724d8c78e56a82a068f80f591655b
Eduardo Barroso <dev@edbar.xyz>
2025-08-30 18:55:19 -0300

    change vim markdown preview plugin to markview

---
+++ config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,8 @@
+return { -- A hackable markdown, Typst, latex, html(inline) & YAML previewer for Neovim
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	priority = 49,
+	dependencies = {
+		"saghen/blink.cmp",
+	},
+}
--- config/nvim/lua/plugins/preview.lua
+++
@@ -1,8 +0,0 @@
-return { -- Neovim wrapper around MD-TUI
-	"henriklovhaug/Preview.nvim",
-	cmd = { "Preview" },
-	config = function()
-		require("preview").setup()
-	end,
-}
-
