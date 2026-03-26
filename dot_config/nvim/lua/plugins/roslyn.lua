df66d878608c8afcde53700fb7a82a818f0c9089
Eduardo Barroso <edbar42@proton.me>
2026-03-26 20:24:17 -0300

    better dotnet version matching with roslyn

--- dot_config/nvim/lua/plugins/roslyn.lua
+++ dot_config/nvim/lua/plugins/roslyn.lua
@@ -5,7 +5,15 @@ return { -- Roslyn LSP plugin for neovim
     ft = { "cs", "razor" },
     opts = {},
     init = function()
-        local dotnet_root = "/home/edbar/.local/share/mise/installs/dotnet/10.0.102"
+        local dotnet_base = "/home/edbar/.local/share/mise/installs/dotnet"
+        local dotnet_root = vim.fn.glob(dotnet_base .. "/10.*")
+        if dotnet_root == "" then
+            vim.notify("Roslyn: no dotnet 10.x found in " .. dotnet_base, vim.log.levels.ERROR)
+            return
+        end
+        -- If glob returns multiple matches (newline-separated), take the last (latest)
+        dotnet_root = vim.split(dotnet_root, "\n")
+        dotnet_root = dotnet_root[#dotnet_root]
         vim.lsp.config("roslyn", {
             cmd_env = {
                 DOTNET_ROOT = dotnet_root,
