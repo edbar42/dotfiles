8ec8f623b68b8729e5404943bd298384b274b524
Eduardo Barroso <edbar42@proton.me>
2026-07-29 23:51:56 -0300

    add disable.lua

--- dot_config/nvim/lua/plugins/disable.lua
+++ dot_config/nvim/lua/plugins/disable.lua
@@ -2,6 +2,18 @@ return { -- Disabling plugins
   { "akinsho/bufferline.nvim", enabled = false },
   { "ibhagwan/fzf-lua.nvim", enabled = false },
   {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+    keys = {
+      { "<leader>e", false },
+      { "<leader>E", false },
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
