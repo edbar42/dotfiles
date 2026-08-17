52678bcc301b195e7e6aadf4d808a13bf48720da
Eduardo Barroso <edbar42@proton.me>
2026-08-16 23:38:21 -0300

    media keys on quattro

--- dot_config/hypr/keybinds.lua
+++ dot_config/hypr/keybinds.lua
@@ -72,8 +72,7 @@ hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl s 10%-"), { lock
 -- Drag floating windows with your mouse
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 
--- Requires playerctl
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("omarchy-shell media next"), { locked = true })
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("omarchy-shell media playPause"), { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("omarchy-shell media playPause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("omarchy-shell media previous"), { locked = true })
